5b1d139d21f43829511a65c7217e67393804584b
test-one9 <Abdulghaniichbin121@outlook.com>
2026-08-31 11:11:16 +0200

    Add files via upload

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,27 +1,39 @@
-# device/sony/ps4/mkbootimg.mk
+#
+# Copyright (C) 2026 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 LOCAL_PATH := $(call my-dir)
 
-# Define the output names
+# PS4 uses a custom Linux/kexec based boot sequence rather than standard Android fastboot.
+# This mkbootimg.mk overrides the default target to package the bzImage (kernel) 
+# and ramdisk into a format suitable for the PS4 Linux loaders (e.g., Orbis/Payloads).
+
+INTERNAL_BOOTIMAGE_ARGS := 	--kernel $(PRODUCT_OUT)/kernel 	--ramdisk $(PRODUCT_OUT)/ramdisk.img 	--cmdline "$(BOARD_KERNEL_CMDLINE)" 	--base $(BOARD_KERNEL_BASE) 	--pagesize $(BOARD_KERNEL_PAGESIZE)
+
+ifeq ($(BOARD_USES_RECOVERY_AS_BOOT),true)
+INTERNAL_BOOTIMAGE_ARGS += --recovery_as_boot
+endif
+
+ifdef BOARD_INCLUDE_DTB
+INTERNAL_BOOTIMAGE_ARGS += --dtb $(PRODUCT_OUT)/dtb.img
+endif
+
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
 
-# The PS4 usually boots via a bzImage and an initrd.img
-# We combine them here so the build system outputs a flashable/loadable file.
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(BOOTIMAGE_EXTRA_DEPS)
 	$(call pretty,"Target boot image: $@")
-	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) \
-		--kernel $(PRODUCT_OUT)/kernel \
-		--ramdisk $(PRODUCT_OUT)/ramdisk.img \
-		--cmdline "$(BOARD_KERNEL_CMDLINE)" \
-		--base $(BOARD_KERNEL_BASE) \
-		--pagesize $(BOARD_KERNEL_PAGESIZE) \
-		--output $@
-	@echo "--- Made PS4 Boot Image: $@ ---"
-
-# Create a specialized PS4-kexec package (bzImage + initrd)
-.PHONY: ps4_kexec_package
-ps4_kexec_package: $(INSTALLED_BOOTIMAGE_TARGET)
-	@mkdir -p $(PRODUCT_OUT)/ps4_boot
-	cp $(PRODUCT_OUT)/kernel $(PRODUCT_OUT)/ps4_boot/bzImage
-	cp $(PRODUCT_OUT)/ramdisk.img $(PRODUCT_OUT)/ps4_boot/initrd.img
-	@echo "--- PS4 Kexec files ready in $(PRODUCT_OUT)/ps4_boot ---"
+	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	@echo "==== PS4 Boot Image Built: $@ ===="
+	@echo "For PS4 kexec injection, utilize bzImage and ramdisk.img directly or pack via payload."
